36d9307b375495fd7b8f5321b248d81dd7e6943f
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2025-04-01 01:11:43 +0200

    findUser() method done - JUnit

--- src/esercizi/AccountManagementTest.java
+++ src/esercizi/AccountManagementTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Optional;
 import java.util.Set;
 import java.util.TreeSet;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,7 +20,7 @@ class AccountManagementTest {
 	void setUpBeforeClass() throws Exception {
 		am = new AccountManagement(pathFile);
 		
-		User testUser = new User();
+		testUser = new User();
 		testUser.setId("U111");
 		testUser.setName("Matteo");
 		testUser.setLastname("Ruggieri");
@@ -73,7 +74,14 @@ class AccountManagementTest {
 	
 	@Test
 	void findUser() {
+		am.addUser(testUser.getId(), testUser.getName(), testUser.getLastname(), testUser.getAddress());
+		Optional<User> result = am.user(testUser.getId());
+		assertEquals(testUser, result.get());
 		
+		/* Normalmente "assertEquals" confronta i riferimenti in
+		 * memoria degli oggetti utilizzano equals(), ma in questo caso,
+		 * nella classe User è stato sovrascritto il metodo equals
+		 * che ora confronta il valore degli attributi. */
 	}
 
 
